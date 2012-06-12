c5a01e3d7f5e0444675c44ac8cab7e724b27fcd0
qianxiaobo <xiaobo.qian@csst.com>
2012-06-12 10:23:00 +0800

    this is one

---
+++ show-me/src/com/test/ShowMe.java
@@ -0,0 +1,20 @@
+package com.test;
+
+/**
+ * @ClassName: ShowMe
+ * @Description: 
+ * @author qian_xiaobo
+ * @date 2012-6-12 上午10:22:22
+ * 
+ */
+public class ShowMe {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
