7fef6d6567aa147fac7ef320d3a4507b2795e692
qianxiaobo <xiaobo.qian@csst.com>
2012-06-12 10:27:12 +0800

    ddd

---
+++ show-me/src/com/test/NextOne.java
@@ -0,0 +1,21 @@
+package com.test;
+
+/**
+ * @ClassName: NextOne
+ * @Description: 
+ * @author qian_xiaobo
+ * @date 2012-6-12 上午10:26:46
+ * 
+ */
+public class NextOne {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 System.out.println("this is next");
+		 
+
+	}
+
+}
